d528e416eb9fee85f81ed53b30b26496e45edeee
Richard <richardto180@gmail.com>
2023-12-21 13:30:55 -0500

    Added event listener to the add book button to make the form popup

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-const bookCard = document.createElement('div');
-bookCard.classList.add('book-card');
+const addBookBtn = document.querySelector('.add-book-btn');
+const bookForm = document.querySelector('#book-form');
 
-const bookInfo = document.createElement('div');
-bookInfo.classList.add('book-info');
-bookCard.appendChild(bookInfo);
-
-const bookTitle = document.createElement('p');
-bookTitle.classList.add('book-title');
-bookTitle.textContent = `Title: ${this.title}`;
-
-const bookAuthor = document.createElement('p');
-bookAuthor.classList.add('book-author');
-bookAuthor.textContent = `Author: ${this.author}`;
-
-const bookPages = document.createElement('p');
-bookPages.classList.add('book-pg');
-bookAuthor.textContent = `Pages: ${this.pages}`;
-
-
-console.log(bookCard);
-console.log(bookInfo);
+addBookBtn.addEventListener('click', () => {
+  bookForm.style.display = 'block';
+});
 
 const myLibrary = [];
 
@@ -27,8 +11,9 @@ function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  // this.read =
 }
 
 function addBookToLibrary() {
-
+  
 }
