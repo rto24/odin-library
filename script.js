f97ea7064e48d38fa3af3ee38b8552dfc6fe6bf1
Richard <richardto180@gmail.com>
2023-12-21 17:14:31 -0500

    Edit addBookToLibrary function to add classes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const addBookBtn = document.querySelector('.add-book-btn');
 const bookForm = document.querySelector('#book-form');
 const closeForm = document.querySelector('.close-form');
-const bookCard = document.querySelector('.book-card');
+// const bookCard = document.querySelector('.book-card');
+const content = document.querySelector('.content');
 
 addBookBtn.addEventListener('click', () => {
   bookForm.style.display = 'block';
@@ -31,6 +32,7 @@ let library = new Library();
 
 function addBookToLibrary() {
   for (let i = 0; i < library.book.length; i++) {
+    let bookCard = document.createElement('div');
     let bookInfo = document.createElement('div');
     let title = document.createElement('p');
     let author = document.createElement('p');
@@ -41,10 +43,16 @@ function addBookToLibrary() {
     title.textContent = libraryTitle;
     author.textContent = libraryAuthor;
     pages.textContent = libraryPages;
+    content.appendChild(bookCard);
+    bookCard.classList.add('book-card');
     bookCard.appendChild(bookInfo);
+    bookInfo.classList.add('book-info');
     bookInfo.appendChild(title);
+    title.classList.add('book-title');
     bookInfo.appendChild(author);
+    author.classList.add('book-author');
     bookInfo.appendChild(pages);
+    pages.classList.add('book-pg');
   }
 }
 
